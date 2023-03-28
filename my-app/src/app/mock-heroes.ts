916b608b490a431a84c212d9dd2504e6a4ea9956
Juan Ivorra <juan.ivorra11@gmail.com>
2023-03-28 22:06:12 +0200

    Un heroe no es heroe sin sus compis

---
+++ my-app/src/app/mock-heroes.ts
@@ -0,0 +1,14 @@
+import { Hero } from './hero';
+
+export const HEROES: Hero[] = [
+  { id: 11, name: 'Batman' },
+  { id: 12, name: 'Hulk' },
+  { id: 13, name: 'Superman' },
+  { id: 14, name: 'Eltiraflechas' },
+  { id: 15, name: 'Capitanga America' },
+  { id: 16, name: 'Scarlett Johanson' },
+  { id: 17, name: 'Ironmandingo' },
+  { id: 18, name: 'Tordo Raro' },
+  { id: 19, name: 'Elbuenaonda' },
+  { id: 20, name: 'Tornado' }
+];
