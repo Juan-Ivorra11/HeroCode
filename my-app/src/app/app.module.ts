bfae5e8c03d32b4b438603a8397f1428e16637f1
Juan <juan.ivorra11@gmail.com>
2023-03-29 12:05:11 +0200

    Mensajes de servicios

--- my-app/src/app/app.module.ts
+++ my-app/src/app/app.module.ts
@@ -1,20 +1,22 @@
-import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import { FormsModule } from '@angular/forms'; // <-- NgModel lives here
+import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
 import { HeroesComponent } from './heroes/heroes.component';
 import { HeroDetailComponent } from './hero-detail/hero-detail.component';
+import { MessagesComponent } from './messages/messages.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeroesComponent,
-    HeroDetailComponent
+    HeroDetailComponent,
+    MessagesComponent
   ],
   imports: [
     BrowserModule,
-      FormsModule
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- my-app/src/app/hero-detail/hero-detail.component.ts
+++ my-app/src/app/hero-detail/hero-detail.component.ts
@@ -1,11 +1,17 @@
-import { Hero } from '../hero';
 import { Component, OnInit, Input } from '@angular/core';
+import { Hero } from '../hero';
 
 @Component({
   selector: 'app-hero-detail',
   templateUrl: './hero-detail.component.html',
   styleUrls: ['./hero-detail.component.css']
 })
-export class HeroDetailComponent {
-  @Input() hero: Hero;
+export class HeroDetailComponent implements OnInit {
+  @Input() hero!: Hero;
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
 }
