4049e276ca42cbf0b271b084ab58be7f9ff13bb3
Juan <juan.ivorra11@gmail.com>
2023-03-30 11:02:13 +0200

    Por fin arreglado

--- my-app/src/app/heroes/heroes.component.ts
+++ my-app/src/app/heroes/heroes.component.ts
@@ -9,7 +9,8 @@ import { HeroService } from '../hero.service';
   styleUrls: ['./heroes.component.css']
 })
 export class HeroesComponent implements OnInit {
-  heroes!: Hero[];
+  heroes: Hero[]=[];
+  
 
   constructor(private heroService: HeroService) { }
 
